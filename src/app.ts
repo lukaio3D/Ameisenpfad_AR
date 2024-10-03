252af2a6f407e778041116ae2f7e350394cb132e
Lukas Schaab <lukas.schaab@lukaio.de>
2024-10-03 11:43:09 +0200

    added webxrdefaultexperience

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
 import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
+import { WebXRDefaultExperience } from '@babylonjs/core/XR/webXRDefaultExperience.js'
 
 class App {
     constructor() {
