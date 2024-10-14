2471e0c0ea3f422b49275def473d5f494bf40d4f
Lukas Schaab <lukas.schaab@lukaio.de>
2024-10-14 13:25:42 +0200

    now working glb ant model to place with hit test

--- src/app.ts
+++ src/app.ts
@@ -21,103 +21,101 @@ class App {
         var engine = new Engine(canvas, true);
         var scene = new Scene(engine);
 
-    // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new Scene(engine);
+        // This creates a basic Babylon Scene object (non-mesh)
+        var scene = new Scene(engine);
 
-    // This creates and positions a free camera (non-mesh)
-    var camera = new FreeCamera("camera1", new Vector3(0, 1, -5), scene);
+        // This creates and positions a free camera (non-mesh)
+        var camera = new FreeCamera("camera1", new Vector3(0, 1, -5), scene);
 
-    // This targets the camera to scene origin
-    camera.setTarget(Vector3.Zero());
+        // This targets the camera to scene origin
+        camera.setTarget(Vector3.Zero());
 
-    // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
+        // This attaches the camera to the canvas
+        camera.attachControl(canvas, true);
 
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+        var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
 
-    // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
+        // Default intensity is 1. Let's dim the light a small amount
+        light.intensity = 0.7;
 
-    var dirLight = new DirectionalLight('light', new Vector3(0, -1, -0.5), scene);
-    dirLight.position = new Vector3(0, 5, -5);
+        var dirLight = new DirectionalLight('light', new Vector3(0, -1, -0.5), scene);
+        dirLight.position = new Vector3(0, 5, -5);
 
-    var shadowGenerator = new ShadowGenerator(1024, dirLight);
-    shadowGenerator.useBlurExponentialShadowMap = true;
-    shadowGenerator.blurKernel = 32;
+        var shadowGenerator = new ShadowGenerator(1024, dirLight);
+        shadowGenerator.useBlurExponentialShadowMap = true;
+        shadowGenerator.blurKernel = 32;
 
 /*     const model = await SceneLoader.ImportMeshAsync("", "assets/", "240920_AntAnim.glb", scene);
  */    const container = await loadAssetContainerAsync("assets/240920_AntAnim.glb", scene);
-    const model = container.meshes[0];
-    const idleRange = { from: 0, to: 100 }; // Define idleRange with appropriate values
+        const model = container.meshes[1];
 
-    var xr = await scene.createDefaultXRExperienceAsync({
-        uiOptions: {
-            sessionMode: "immersive-ar",
-            referenceSpaceType: "local-floor"
-        },
-        optionalFeatures: true
-    });
-
-    const fm = xr.baseExperience.featuresManager;
-
-    const xrTest = fm.enableFeature(WebXRHitTest.Name, "latest") as WebXRHitTest;
-    const anchors = fm.enableFeature(WebXRAnchorSystem.Name, 'latest');
-
-    let b = model /* CreateCylinder('cylinder', { diameterBottom: 0.2, diameterTop: 0.4, height: 0.5 }); */
-    b.rotationQuaternion = new Quaternion();
-    // b.isVisible = false;
-    shadowGenerator.addShadowCaster(b, true);
-
-    const marker = MeshBuilder.CreateTorus('marker', { diameter: 0.05, thickness: 0.02 });
-    marker.isVisible = false;
-    marker.rotationQuaternion = new Quaternion();
-
-    let hitTest;
-
-    b.isVisible = false;
-
-    (xrTest as WebXRHitTest).onHitTestResultObservable.add((results) => {
-        if (results.length) {
-            marker.isVisible = true;
-            hitTest = results[0];
-            hitTest.transformationMatrix.decompose(undefined, b.rotationQuaternion, b.position);
-            hitTest.transformationMatrix.decompose(undefined, marker.rotationQuaternion, marker.position);
-        } else {
-            marker.isVisible = false;
-            hitTest = undefined;
-        }
-    });
-    const mat1 = new StandardMaterial('1', scene);
-    mat1.diffuseColor = Color3.Red();
-    const mat2 = new StandardMaterial('1', scene);
-    mat2.diffuseColor = Color3.Blue();
-
-    if (anchors) {
-        console.log('anchors attached');
-        (anchors as WebXRAnchorSystem).onAnchorAddedObservable.add(anchor => {
-            console.log('attaching', anchor);
-            b.isVisible = true;
-            anchor.attachedNode = b.clone("mensch", null, true);
-            shadowGenerator.addShadowCaster(anchor.attachedNode as Mesh, true);
-            scene.beginAnimation((anchor.attachedNode as Mesh).skeleton, idleRange.from, idleRange.to, true);
-            b.isVisible = false;
+        var xr = await scene.createDefaultXRExperienceAsync({
+            uiOptions: {
+                sessionMode: "immersive-ar",
+                referenceSpaceType: "local-floor"
+            },
+            optionalFeatures: true
         });
 
-        (anchors as WebXRAnchorSystem).onAnchorRemovedObservable.add(anchor => {
-            console.log('disposing', anchor);
-            if (anchor) {
-                (anchor.attachedNode as Mesh).isVisible = false;
-                anchor.attachedNode.dispose();
+        const fm = xr.baseExperience.featuresManager;
+
+        const xrTest = fm.enableFeature(WebXRHitTest.Name, "latest") as WebXRHitTest;
+        const anchors = fm.enableFeature(WebXRAnchorSystem.Name, 'latest');
+
+        let b = model /* CreateCylinder('cylinder', { diameterBottom: 0.2, diameterTop: 0.4, height: 0.5 }); */
+        b.rotationQuaternion = new Quaternion();
+        // b.isVisible = false;
+        shadowGenerator.addShadowCaster(b, true);
+
+        const marker = MeshBuilder.CreateTorus('marker', { diameter: 0.05, thickness: 0.02 });
+        marker.isVisible = false;
+        marker.rotationQuaternion = new Quaternion();
+
+        let hitTest;
+
+        b.isVisible = false;
+
+        (xrTest as WebXRHitTest).onHitTestResultObservable.add((results) => {
+            if (results.length) {
+                marker.isVisible = true;
+                hitTest = results[0];
+                hitTest.transformationMatrix.decompose(undefined, b.rotationQuaternion, b.position);
+                hitTest.transformationMatrix.decompose(undefined, marker.rotationQuaternion, marker.position);
+            } else {
+                marker.isVisible = false;
+                hitTest = undefined;
             }
         });
-    }
+        const mat1 = new StandardMaterial('1', scene);
+        mat1.diffuseColor = Color3.Red();
+        const mat2 = new StandardMaterial('1', scene);
+        mat2.diffuseColor = Color3.Blue();
 
-    scene.onPointerDown = (evt, pickInfo) => {
-        if (hitTest && anchors && xr.baseExperience.state === WebXRState.IN_XR) {
-            (anchors as WebXRAnchorSystem).addAnchorPointUsingHitTestResultAsync(hitTest);
+        if (anchors) {
+            console.log('anchors attached');
+            (anchors as WebXRAnchorSystem).onAnchorAddedObservable.add(anchor => {
+                console.log('attaching', anchor);
+                b.isVisible = true;
+                anchor.attachedNode = b.clone("mensch", null, true);
+                shadowGenerator.addShadowCaster(anchor.attachedNode as Mesh, true);
+                b.isVisible = false;
+            });
+
+            (anchors as WebXRAnchorSystem).onAnchorRemovedObservable.add(anchor => {
+                console.log('disposing', anchor);
+                if (anchor) {
+                    (anchor.attachedNode as Mesh).isVisible = false;
+                    anchor.attachedNode.dispose();
+                }
+            });
         }
-    }
+
+        scene.onPointerDown = (evt, pickInfo) => {
+            if (hitTest && anchors && xr.baseExperience.state === WebXRState.IN_XR) {
+                (anchors as WebXRAnchorSystem).addAnchorPointUsingHitTestResultAsync(hitTest);
+            }
+        }
 
         // run the main render loop
         engine.runRenderLoop(() => {
