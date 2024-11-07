5bff211fbe3cadc142316c0529b0196aafbefb73
Lukas Schaab <lukas.schaab@lukaio.de>
2024-11-07 14:11:49 +0100

    added navmesh

--- src/app.ts
+++ src/app.ts
@@ -33,10 +33,12 @@ import {
   CylinderBuilder,
   CreateCylinder,
   loadAssetContainerAsync,
-  RecastJSPlugin,
   TransformNode,
+  RecastJSPlugin,
 } from "@babylonjs/core";
 import { WebXRDefaultExperience } from "@babylonjs/core/XR/webXRDefaultExperience.js";
+import Recast from "recast-detour";
+import { Inspector } from "@babylonjs/inspector";
 
 class App {
   constructor() {
@@ -59,7 +61,14 @@ class App {
     var scene = new Scene(engine);
 
     // This creates and positions a free camera (non-mesh)
-    var camera = new FreeCamera("camera1", new Vector3(0, 1, -5), scene);
+    var camera = new ArcRotateCamera(
+      "camera",
+      -Math.PI / 2,
+      Math.PI / 2.5,
+      5,
+      Vector3.Zero(),
+      scene
+    );
 
     // This targets the camera to scene origin
     camera.setTarget(Vector3.Zero());
@@ -91,80 +100,138 @@ class App {
       scene
     );
     container.addAllToScene();
-    console.log(container);
     const ant = container.meshes[0];
-    console.log(ant);
     ant.scaling = new Vector3(2, 2, 2);
-    console.log(ant);
 
     const antAnim = scene.getAnimationGroupByName("Armature Ant");
     antAnim.stop(true);
 
-    let xr = await scene.createDefaultXRExperienceAsync({
-      uiOptions: {
-        sessionMode: "immersive-ar",
-        referenceSpaceType: "local-floor",
-      },
-      optionalFeatures: true,
-    });
+    //Navmesh Setup
+    let recast = await Recast();
+    let navigationPlugin = new RecastJSPlugin();
 
-    const fm = xr.baseExperience.featuresManager;
-
-    const xrTest = fm.enableFeature(WebXRHitTest, "latest") as WebXRHitTest;
-    const anchors = fm.enableFeature(
-      WebXRAnchorSystem,
-      "latest"
-    ) as WebXRAnchorSystem;
-
-    // a dot to show in the found position
-    const dot = SphereBuilder.CreateSphere(
-      "dot",
-      {
-        diameter: 0.05,
-      },
+    const groundMesh = MeshBuilder.CreateGround(
+      "ground",
+      { width: 10, height: 10 },
       scene
     );
 
-    let hitTest;
-    let antToBePlaced = true;
+    const navParameters = {
+      cs: 0.2,
+      ch: 0.2,
+      walkableSlopeAngle: 35,
+      walkableHeight: 1,
+      walkableClimb: 1,
+      walkableRadius: 1,
+      maxEdgeLen: 12,
+      maxSimplificationError: 1.3,
+      minRegionArea: 8,
+      mergeRegionArea: 20,
+      maxVertsPerPoly: 6,
+      detailSampleDist: 6,
+      detailSampleMaxError: 1,
+    };
 
-    dot.isVisible = false;
-    ant.isVisible = false;
+    navigationPlugin.createNavMesh([groundMesh], navParameters);
 
-    xrTest.onHitTestResultObservable.add((results) => {
-      if (results.length && antToBePlaced) {
-        hitTest = results[0];
-        ant.isVisible = true;
-        results[0].transformationMatrix.decompose(
-          undefined,
-          ant.rotationQuaternion,
-          ant.position
-        );
-      } else {
-        ant.isVisible = false;
-        hitTest = null; // Reset hitTest if no results
-      }
+    var navmeshdebug = navigationPlugin.createDebugNavMesh(scene);
+    navmeshdebug.position = new Vector3(0, 0.01, 0);
+
+    var matdebug = new StandardMaterial("matdebug", scene);
+    matdebug.diffuseColor = new Color3(0.1, 0.2, 1);
+    matdebug.alpha = 0.2;
+    navmeshdebug.material = matdebug;
+
+    // crowd
+    var crowd = navigationPlugin.createCrowd(10, 0.1, scene);
+    let antTransform = new TransformNode("antTransform", scene);
+    const box = MeshBuilder.CreateBox("box", { size: 0.1 }, scene);
+    box.parent = antTransform;
+    ant.parent = antTransform;
+    var agentParams = {
+      radius: 0.1,
+      height: 0.2,
+      maxAcceleration: 4.0,
+      maxSpeed: 1.0,
+      collisionQueryRange: 0.5,
+      pathOptimizationRange: 0.0,
+      separationWeight: 1.0,
+    };
+    let endPoint = new Vector3(1, 0, 0);
+
+    const agentIndex = crowd.addAgent(new Vector3(0, 0, 0), agentParams, antTransform);
+    crowd.agentGoto(agentIndex, navigationPlugin.getClosestPoint(endPoint)); 
+
+    // AR Setup
+    // let xr = await scene.createDefaultXRExperienceAsync({
+    //   uiOptions: {
+    //     sessionMode: "immersive-ar",
+    //     referenceSpaceType: "local-floor",
+    //   },
+    //   optionalFeatures: true,
+    // });
+
+    // const fm = xr.baseExperience.featuresManager;
+
+    // const xrTest = fm.enableFeature(WebXRHitTest, "latest") as WebXRHitTest;
+    // const anchors = fm.enableFeature(
+    //   WebXRAnchorSystem,
+    //   "latest"
+    // ) as WebXRAnchorSystem;
+
+    // // a dot to show in the found position
+    // const dot = SphereBuilder.CreateSphere(
+    //   "dot",
+    //   {
+    //     diameter: 0.05,
+    //   },
+    //   scene
+    // );
+
+    // let hitTest;
+    // let antToBePlaced = true;
+
+    // dot.isVisible = false;
+    // ant.isVisible = false;
+
+    // xrTest.onHitTestResultObservable.add((results) => {
+    //   if (results.length && antToBePlaced) {
+    //     hitTest = results[0];
+    //     ant.isVisible = true;
+    //     results[0].transformationMatrix.decompose(
+    //       undefined,
+    //       ant.rotationQuaternion,
+    //       ant.position
+    //     );
+    //   } else {
+    //     ant.isVisible = false;
+    //     hitTest = null; // Reset hitTest if no results
+    //   }
+    // });
+
+    // scene.onPointerDown = (evt, pickInfo) => {
+    //   if (
+    //     antToBePlaced &&
+    //     hitTest &&
+    //     anchors &&
+    //     xr.baseExperience.state === WebXRState.IN_XR
+    //   ) {
+    //     anchors.addAnchorPointUsingHitTestResultAsync(hitTest);
+    //     antToBePlaced = false;
+    //   }
+    //   if(!antToBePlaced && pickInfo.pickedMesh === ant) {
+    //     console.log("start Animation");
+    //     if(!antAnim.isPlaying) {
+    //     antAnim.start(true);
+    //   } else {
+    //     console.log("stop Animation");
+    //     antAnim.stop(true);
+    //   }
+    // }};
+
+    Inspector.Show(scene, {
+      embedMode: true,
     });
-
-    scene.onPointerDown = (evt, pickInfo) => {
-      if (
-        antToBePlaced &&
-        hitTest &&
-        anchors &&
-        xr.baseExperience.state === WebXRState.IN_XR
-      ) {
-        anchors.addAnchorPointUsingHitTestResultAsync(hitTest);
-        antToBePlaced = false;
-      }
-      if(!antToBePlaced && pickInfo.pickedMesh === ant) {
-        console.log("start Animation");
-        if(!antAnim.isPlaying) {
-        antAnim.start(true);
-      } else {
-        console.log("stop Animation");
-        antAnim.stop(true);
-      }
-    }};
 
     // run the main render loop
     engine.runRenderLoop(() => {
