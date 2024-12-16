bdb85ab0f2997c14413c5f4e7354cc272cb6d02a
Lukas Schaab <lukas.schaab@lukaio.de>
2024-12-16 11:59:18 +0100

    removed angular sensitivity

--- src/Features/Camera.ts
+++ src/Features/Camera.ts
@@ -25,7 +25,6 @@ export default async function createCamera(canvas, scene) {
     );
     camera.setTarget(new Vector3(0, 0, 1));
     // Sets the sensitivity of the camera to movement and rotation
-    camera.angularSensibility = 10000; // Erhöhen für weniger Empfindlichkeit
     camera.inertia = 0.95; // Höherer Wert für glattere Bewegung
     camera.attachControl(canvas, true);
 
