bcedb9743b6c4753fbc5a1154999a3213937625d
Lukas Schaab <lukas.schaab@lukaio.de>
2025-02-08 12:28:59 +0100

    dsfsdf

--- src/Features/Camera.ts
+++ src/Features/Camera.ts
@@ -77,9 +77,9 @@ export default async function createCamera(
       // Kamera-Einstellungen Android
       camera.fov = 0.7;
       camera.minZ = 0.1;
-      camera.inertia = 0.1;
+      camera.inertia = 0.15;
       camera.angularSensibility = 2000;
-      smoothFactor = 0.2;
+      smoothFactor = 0.15;
     }
 
     // Touch Controls aktivieren
