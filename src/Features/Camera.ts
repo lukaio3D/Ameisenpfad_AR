d3cf3726b934592ec60b1a254475dcb5bd6f8041
Lukas Schaab <lukas.schaab@lukaio.de>
2025-02-14 11:43:38 +0100

    enable debug panel again

--- src/Features/Camera.ts
+++ src/Features/Camera.ts
@@ -109,5 +109,16 @@ export default async function createCamera(
     skybox.dispose();
   }
 
+   // Debug GUI mit dat.gui für inertia und angularSensibility
+   const gui = new dat.GUI();
+   gui.add(camera, "inertia", 0, 1)
+     .step(0.01)
+     .name("Inertia")
+     .listen();
+   gui.add(camera, "angularSensibility", 0, 5000)
+     .step(10)
+     .name("Angular Sensibility")
+     .listen();
+
   return { camera, skybox };
 }
